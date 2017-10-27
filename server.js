32b3b167955b8e684f7b60afebf6f6dd06eac61d
gnenov89 <georgi.nenov89@gmail.com>
2017-10-27 08:31:39 -0400

    serverInProgress

---
+++ server.js
@@ -0,0 +1,30 @@
+// Dependencies
+var express = require("express");
+var bodyParser = require("body-parser");
+var logger = require("morgan");
+var hbs = require("express-handlebars");
+
+// Require the routes and use them 
+var routes = require("./ruotes/routes");
+
+// Initialize express
+var app = express();
+
+// set the port
+var port = process.env.PORT ||3000;
+
+// set up the HBS view engine
+app.engine('hbs', hbs({defaultLayout: 'main', extname: 'hbs', partialsDir: [__dirname + '/views/partials']}));
+app.set('view engine', 'hbs');
+
+// User morgan for debug loggin
+app.use(logger("dev"));
+
+// set up body-parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+
+// set up the public direnctrory as static
+app.
