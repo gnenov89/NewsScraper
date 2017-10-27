607a2296cfe7f00195af71aa6c12f453473775e0
gnenov89 <georgi.nenov89@gmail.com>
2017-10-27 11:33:35 -0400

    notes ready to go

--- models/Note.js
+++ models/Note.js
@@ -1,21 +1,25 @@
-// Require mogoose
+// Require mongoose
 var mongoose = require("mongoose");
 
-// require the connection 
+// require the connection
 var db = require("../config/connection");
 
 // Create a schema class
 var Schema = mongoose.Schema;
 
-// Create the Note schema 
+// Create the Note schema
 var NoteSchema = new Schema({
-    // Just a string 
-    moreText:{
-        type:String
-    }
+  // Just a string
+  noteText: {
+    type: String
+  }
 });
 
 // Remember, Mongoose will automatically save the ObjectIds of the notes
 // These ids are referred to in the Article model
-var Note = mongoose.model("Note", NotEChema);
 
+// Create the Note model with the NoteSchema
+var Note = mongoose.model("Note", NoteSchema);
+
+// Export the Note model
+module.exports = Note;
