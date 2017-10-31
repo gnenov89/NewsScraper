9caba559da1a47dfcb9ef8d30ccea5ef079f3438
gnenov89 <georgi.nenov89@gmail.com>
2017-10-31 08:39:20 -0400

    Adding NoteFunction

--- public/javascript/custom.js
+++ public/javascript/custom.js
@@ -4,3 +4,28 @@ $(".button-collapse").sideNav();
     // define the modal 
 $("#noteModal").modal({
 });
+
+// set up the onclick for the buttons for each note 
+$(".noteButton").on("click", function(ret){
+
+    // if we have duplicate listener, stop it from listening 
+    ret.stopImmediatePropagation();
+
+    // select to work with 
+    var currentButton = $(this).attr("id");
+
+        // call the populateNote function for the button 
+        populateNote(currentButton);
+
+        // open the modal
+        $("noteModal").modal("open");
+
+        // set up response of clicking the noteButton
+        $("#noteButton").on("click", function(ret){
+
+
+        });
+    }
+    
+    });
+    
